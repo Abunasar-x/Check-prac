bf3df5df04ddf4939d731056213416053b4165d9
Abunasar-x <abunasar.aq@gmail.com>
2026-05-23 15:27:43 +0500

    Added code

---
+++ Second task/app.py
@@ -0,0 +1,20 @@
+# Simple Calculator Program
+
+# Take input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Perform operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+remainder = num1 % num2
+
+# Display results
+print("\nResults:")
+print("Addition =", addition)
+print("Subtraction =", subtraction)
+print("Multiplication =", multiplication)
+print("Division =", division)
+print("Remainder =", remainder)
